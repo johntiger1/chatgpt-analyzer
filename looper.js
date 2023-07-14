4e208127d52fb11b9bf1d8e00e79cb2e270fa25f
John Chen <john.person1@gmail.com>
2023-07-14 17:32:09 -0400

    clean up code

--- looper.js
+++ looper.js
@@ -1,37 +1,6 @@
 
 import { stopwords } from './stopwords.js';
 
-chats = document.querySelectorAll('div.relative:nth-child(4) > ol:nth-child(2) li a');
-
-chats.forEach((li, i) => {  
-  
-    setTimeout(() => {
-          li.click();  
-      const text = li.querySelector('.flex-1').textContent.trim();
-      console.log(text);  
-    }, 2000 * i);
-  })
-
-
-
-//   document.querySelector("#__next > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div.dark.flex-shrink-0.overflow-x-hidden.bg-gray-900 > div > div > div > nav > div.flex-col.flex-1.transition-opacity.duration-500.overflow-y-auto.-mr-2 > div > div > span:nth-child(1) > div:nth-child(3) > ol > li:nth-child(5) > a")
-
-
-// div.pb-2 > div:nth-child(1) > span:nth-child(1) > div:nth-child(2)
-
-// div.relative:nth-child(3) > div:nth-child(1) > h3:nth-child(1)
-
-
-
-// #__next > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div.dark.flex-shrink-0.overflow-x-hidden.bg-gray-900 > div > div > div > nav > div.flex-col.flex-1.transition-opacity.duration-500.overflow-y-auto.-mr-2 > div > div > span:nth-child(1) > div:nth-child(3) > ol > li:nth-child(5) > a
-
-
-// div.pb-2 > div:nth-child(1) > span:nth-child(1)
-// 
-// all_blocks = '#__next > div.overflow-hidden.w-full.h-full.relative.flex.z-0 > div.dark.flex-shrink-0.overflow-x-hidden.bg-gray-900 > div > div > div > nav > div.flex-col.flex-1.transition-opacity.duration-500.overflow-y-auto.-mr-2 > div > div > span:nth-child(1)'
-
-
-// div.pb-2 > div:nth-child(1) > span:nth-child(2) > div:nth-child(1)
 // history grouping
 const blocks = document.querySelectorAll('div.pb-2 > div:nth-child(1) > span:nth-child(-n+2) > div')
 
@@ -82,18 +51,3 @@ async function processBlocks() {
 processBlocks();
 
 
-// now, select the divs inside 
-// blocks.querySelectorAll('ol > li')
-
-// we can either write a list; or we can just iterate do them 
-
-// const all_list_items = []
-// // iterate each block
-// blocks.forEach(block => {
-//     listItems.push(...block.querySelectorAll('ol > li'));
-
-//   });
-
-// const per_block_messages = 
-
-// blocks[0]
