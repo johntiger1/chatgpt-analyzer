b7c9a963cb175d1f398b3ac752219e894708ead7
John Chen <john.person1@gmail.com>
2023-07-15 16:02:56 -0400

    redrawing word cloud each time

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 console.log('aqweq adding event listener')
 
+let layout;
 
 // Add an event listener to the element with the ID 'analyzeButton' that triggers when the button is clicked.
 // The logic for the "analyze" action is held in content.js. 
@@ -61,36 +62,7 @@ console.log('aqweq adding event listener')
 // const width = 500;
 // const height = 500;
 
-// // Word cloud layout
-// const layout = d3.layout.cloud()
-//   .size([width, height])
-//   .words(words)
-//   .padding(5)
-//   .rotate(() => Math.floor(Math.random() * 2) * 90)
-//   .fontSize(d => d.size)
-//   .on('end', draw);
 
-// layout.start();
-
-// // Function to draw the word cloud
-// function draw(words) {
-//   const svg = d3.select('#word-cloud')
-//     .append('svg')
-//     .attr('width', layout.size()[0])
-//     .attr('height', layout.size()[1])
-//     .append('g')
-//     .attr('transform', `translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`);
-
-//   const text = svg.selectAll('text')
-//     .data(words)
-//     .enter()
-//     .append('text')
-//     .style('font-size', d => `${d.size}px`)
-//     .style('fill', (d, i) => d3.schemeCategory10[i % 10])
-//     .attr('text-anchor', 'middle')
-//     .attr('transform', d => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
-//     .text(d => d.text);
-// }
 
   
 //   });
@@ -130,29 +102,76 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     numberOfChats.textContent = "Identified " + request.length + " chats!"
     progressStatusText.textContent = "Beginning processing..."
 
+
+      // Word cloud layout
+      const width = 500;
+      const height = 500;
+
+      layout = d3.layout.cloud()
+        .size([width, height])
+        .padding(5)
+        .rotate(() => Math.floor(Math.random() * 2) * 90)
+        .fontSize(d => d.size)
+        .on('end', draw);
+
+      layout.start();
   }
 
   else if  (request.action=="finishProgress") {
     const progressStatusText = document.getElementById('current-status');
-    progressStatusText.textContent = "Finished processing..."
+    progressStatusText.textContent = "Finished!"
   }
 
   else if (request.action=="wordCounterUpdate"){
-    console.log('updating the word log')
+    console.log('updating the word log');
     let wordList = document.getElementById('wordList');
+  
+    // Clear any existing entries
+    wordList.innerHTML = '';
+  
+    // Add a new li element for each entry in globalWordFrequency
+    for (let [word, frequency] of request.globalWordFrequency) {
+      let listItem = document.createElement('li');
+      listItem.textContent = `${word}: ${frequency}`;
+      wordList.appendChild(listItem);
+    }
+  
+    const wordArray = Array.from(request.globalWordFrequency, ([text, size]) => ({ text, size }));
+  
 
-// Clear any existing entries
-wordList.innerHTML = '';
+    function draw(words) {
+      // Clear any existing word cloud
+      d3.select('#word-cloud').html('');
+    
+      const svg = d3.select('#word-cloud')
+        .append('svg')
+        .attr('width', layout.size()[0])
+        .attr('height', layout.size()[1])
+        .append('g')
+        .attr('transform', `translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`);
+    
+      const text = svg.selectAll('text')
+        .data(words)
+        .enter()
+        .append('text')
+        .style('font-size', d => `${d.size}px`)
+        .style('fill', (d, i) => d3.schemeCategory10[i % 10])
+        .attr('text-anchor', 'middle')
+        .attr('transform', d => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
+        .text(d => d.text);
+    }
+    console.log('hello')
+    if (layout) {
+    console.log('layout exists')
 
-// Add a new li element for each entry in globalWordFrequency
-for (let [word, frequency] of request.globalWordFrequency) {
-  let listItem = document.createElement('li');
-  listItem.textContent = `${word}: ${frequency}`;
-  wordList.appendChild(listItem);
-}
-
+      layout.stop().words(wordArray).start();
+    }
   }
+  
+  
+
 });
+
 
 document.getElementById('start-button').addEventListener('click', () => {
   browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
