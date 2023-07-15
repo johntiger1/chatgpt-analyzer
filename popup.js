48ea00a9286d681dcab3b432220f0fa26be64a20
John Chen <john.person1@gmail.com>
2023-07-15 16:12:43 -0400

    working word cloud iteration

--- popup.js
+++ popup.js
@@ -1,80 +1,43 @@
 console.log('aqweq adding event listener')
 
+
+function draw(words) {
+  // Clear any existing word cloud
+  d3.select('#word-cloud').html('');
+
+  const svg = d3.select('#word-cloud')
+    .append('svg')
+    .attr('width', layout.size()[0])
+    .attr('height', layout.size()[1])
+    .append('g')
+    .attr('transform', `translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`);
+
+  const text = svg.selectAll('text')
+    .data(words)
+    .enter()
+    .append('text')
+    .style('font-size', d => `${d.size}px`)
+    .style('fill', (d, i) => d3.schemeCategory10[i % 10])
+    .attr('text-anchor', 'middle')
+    .attr('transform', d => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
+    .text(d => d.text);
+}
+
+
 let layout;
 
-// Add an event listener to the element with the ID 'analyzeButton' that triggers when the button is clicked.
-// The logic for the "analyze" action is held in content.js. 
-// document.getElementById('analyzeButton').addEventListener('click', () => {
-//   browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-//     browser.tabs.sendMessage(
-//       tabs[0].id,
-//       { action: 'analyze' }
-//     ).then((response) => {
-//       const sortedWordFrequency = Object.entries(response.wordFrequency).sort((a, b) => b[1] - a[1]);
-//       const resultsElement = document.getElementById('results');
-//       resultsElement.innerHTML = '';
-//       // this is populating the popup html dynamically
-//       for (const [word, count] of sortedWordFrequency) {
-//         const listItem = document.createElement('li');
-//         listItem.textContent = `${word}: ${count}`;
-//         resultsElement.appendChild(listItem);
-//       }
-//     });
-//   });
-// });
+// Word cloud layout
+const width = 500;
+const height = 500;
 
-// // Logic for analyzeMe fucntion in add.js 
-// document.getElementById('analyzeMeButton').addEventListener('click', () => {
-//   browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-//     browser.tabs.sendMessage(
-//       tabs[0].id,
-//       { action: 'analyzeMe' }
-//     ).then((response) => {
-//       const sortedWordFrequency = Object.entries(response.wordFrequency).sort((a, b) => b[1] - a[1]);
-//       const resultsElement = document.getElementById('results');
-//       resultsElement.innerHTML = '';
-//       // this is populating the popup html dynamically
-//       for (const [word, count] of sortedWordFrequency) {
-//         const listItem = document.createElement('li');
-//         listItem.textContent = `${word}: ${count}`;
-//         resultsElement.appendChild(listItem);
-//       }
-//     });
-//   });
-// });
+layout = d3.layout.cloud()
+  .size([width, height])
+  .padding(5)
+  .rotate(() => Math.floor(Math.random() * 2) * 90)
+  .fontSize(d => d.size)
+  .on('end', draw);
 
-// glue a button to the wordcloud
-// document.getElementById('wordcloud-button').addEventListener('click', () => 
-
-
-// {
-//   browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
-//     browser.tabs.sendMessage(
-//       tabs[0].id,
-//       { action: 'analyze' }
-//     ).then((response) => {
-//       const sortedWordFrequency = Object.entries(response.wordFrequency).sort((a, b) => b[1] - a[1]);
-//       const words = sortedWordFrequency.map(([text, size]) => {
-//         return { text, size };
-//       });
-
-//        // Dimensions
-// const width = 500;
-// const height = 500;
-
-
-
-  
-//   });
-
-//     });
- 
-
-
-
-
-// });
-
+layout.start();
 
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "progessBarUpdate") {
@@ -103,18 +66,7 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     progressStatusText.textContent = "Beginning processing..."
 
 
-      // Word cloud layout
-      const width = 500;
-      const height = 500;
 
-      layout = d3.layout.cloud()
-        .size([width, height])
-        .padding(5)
-        .rotate(() => Math.floor(Math.random() * 2) * 90)
-        .fontSize(d => d.size)
-        .on('end', draw);
-
-      layout.start();
   }
 
   else if  (request.action=="finishProgress") {
@@ -128,7 +80,8 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
   
     // Clear any existing entries
     wordList.innerHTML = '';
-  
+    
+    
     // Add a new li element for each entry in globalWordFrequency
     for (let [word, frequency] of request.globalWordFrequency) {
       let listItem = document.createElement('li');
@@ -139,28 +92,8 @@ browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const wordArray = Array.from(request.globalWordFrequency, ([text, size]) => ({ text, size }));
   
 
-    function draw(words) {
-      // Clear any existing word cloud
-      d3.select('#word-cloud').html('');
     
-      const svg = d3.select('#word-cloud')
-        .append('svg')
-        .attr('width', layout.size()[0])
-        .attr('height', layout.size()[1])
-        .append('g')
-        .attr('transform', `translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`);
-    
-      const text = svg.selectAll('text')
-        .data(words)
-        .enter()
-        .append('text')
-        .style('font-size', d => `${d.size}px`)
-        .style('fill', (d, i) => d3.schemeCategory10[i % 10])
-        .attr('text-anchor', 'middle')
-        .attr('transform', d => `translate(${[d.x, d.y]})rotate(${d.rotate})`)
-        .text(d => d.text);
-    }
-    console.log('hello')
+    console.log('hello - prelayout')
     if (layout) {
     console.log('layout exists')
 
